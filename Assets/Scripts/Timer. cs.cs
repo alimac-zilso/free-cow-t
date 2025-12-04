6b434ae54f4b46111b8fca5a75071abd04734980
CAMILA LUIZE KARLING NUNES <camila_l_nunes@estudante.sesisenai.org.br>
2025-12-04 09:05:11 -0300

    timer adicionado

---
+++ Assets/Scripts/Timer. cs.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+   [SerializeField] TextMeshProUGUI textoTimer;
+    [SerializeField] float tempoRestante;
+
+
+    public bool acabou = false;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        int minutos = Mathf.FloorToInt(tempoRestante / 60);
+        int segundos = Mathf.FloorToInt(tempoRestante % 60);
+        textoTimer.text = string.Format("{0:00}:{1:00}", minutos, segundos);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (tempoRestante > 0)
+        {
+            tempoRestante = tempoRestante - Time.deltaTime;
+        }
+        else
+        {
+            tempoRestante = 0;
+            //GameOver();
+            textoTimer.color = Color.red;
+            acabou = true;
+            Time.timeScale = 0;
+        }
+
+
+
+
+
+
+        int minutos = Mathf.FloorToInt(tempoRestante / 60);
+        int segundos = Mathf.FloorToInt(tempoRestante % 60);
+        textoTimer.text = string.Format("{0:00}:{1:00}", minutos, segundos);
+    }
+
+}
